547e83056d0c0546e69df7f6b5a3121200d050ce
logotipiwe <german.reus@bk.ru>
2023-06-03 12:14:55 +0600

    use BASE_PATH in routes

--- app.ts
+++ app.ts
@@ -3,23 +3,19 @@ import logger from "morgan";
 import cookieParser from "cookie-parser"
 import webControllers from "./routes/controllers/web";
 import httpContext from "express-http-context";
-import {authMiddleware} from "./routes/auth/authMiddleware";
 import EnvAccessor from "./routes/EnvAccessor";
 
-const app = express();
+EnvAccessor.ensureNecessaryVars()
 
+const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(httpContext.middleware);
 
-// app.use(authMiddleware)
-
 webControllers.forEach((value, key) => {
-    app.use(key, value)
+    app.use(EnvAccessor.getBasePath() + key, value)
 })
-
-EnvAccessor.ensureNecessaryVars()
 
 export default app;
